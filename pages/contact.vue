d90cb3c9bea944ff947d320ffde8c479cfa646d6
Patrick_Williamson <patrickw9080@outlook.com>
2022-06-06 15:57:36 -0400

    Implemented contact page.

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,17 @@
     <div class="row">
       <div class="col">
         <h1>Contact info.</h1>
+        <p>See resume and LinkedIn for phone number and email.</p>
+
+        <p>
+          <b-link
+            href="https://www.linkedin.com/in/patrick-williamson-b88b33201/"
+            >LinkedIn</b-link
+          >
+        </p>
+        <p>
+          <b-link href="https://github.com/Patnonymous">Github</b-link>
+        </p>
       </div>
     </div>
   </div>
