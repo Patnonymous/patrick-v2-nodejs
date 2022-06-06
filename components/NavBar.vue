caab5e078cbcf1c24e3908d279d185cf2758b314
Patrick_Williamson <patrickw9080@outlook.com>
2022-06-06 11:26:09 -0400

    Updated Navbar links active class binding.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,9 +7,15 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/about">About</b-nav-item>
-          <b-nav-item to="/projects">Projects</b-nav-item>
-          <b-nav-item to="/contact">Contact</b-nav-item>
+          <b-nav-item :active="isRouteActive('about')" to="/about"
+            >About</b-nav-item
+          >
+          <b-nav-item :active="isRouteActive('projects')" to="/projects"
+            >Projects</b-nav-item
+          >
+          <b-nav-item :active="isRouteActive('contact')" to="/contact"
+            >Contact</b-nav-item
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -19,4 +25,16 @@
 
 
 <script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    isRouteActive(routeName) {
+      return this.$route.fullPath.includes(routeName);
+    },
+  },
+  computed: {},
+};
 </script>
